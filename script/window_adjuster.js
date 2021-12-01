cb6f451c28868059a22a186d3721df20db6f286d
Bippserino <bobomandic2808@gmail.com>
2021-12-02 00:08:49 +0100

    minor fixes

--- script/window_adjuster.js
+++ script/window_adjuster.js
@@ -1,3 +1,10 @@
-$( document ).ready(() => {
+function disclaimer_min_height() {
     $('.disclaimer').css('min-height',`calc(100vh - ${$('.navbar').outerHeight(true)}px - ${$('.footer').outerHeight(true)}px - ${$('.copyright').outerHeight(true)}px)`)
+}
+
+$( document ).ready(() => {
+    disclaimer_min_height()
+    $(window).resize(() => { 
+        disclaimer_min_height()
+    });
 })
